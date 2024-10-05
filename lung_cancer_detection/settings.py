72dbee50054bcd1a51a37979b092b8686e1450a0
Sarthak Aggarwal <122617137+sarthak98765@users.noreply.github.com>
2024-10-06 04:28:48 +0530

    Update settings.py

--- lung_cancer_detection/settings.py
+++ lung_cancer_detection/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = ['https://try-jy3v.onrender.com']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
